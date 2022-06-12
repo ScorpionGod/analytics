65b900728de146a81439da302eeda78d26eb9359
Shyam Chathuranga <scorpiongodlair@gmail.com>
2022-06-12 17:09:17 +0530

    Add files via upload

---
+++ gatracking.js
@@ -0,0 +1,25 @@
+$("[href*='tel:']").click(function(e) {
+
+  e.preventDefault();
+  var href = $(this).attr('href');
+
+  // tel:
+  if (href.toLowerCase().indexOf("tel:") >= 0) {
+    eventCategory = 'Phone Call Tracking';
+    eventLabel = href.replace('tel:', '');
+
+  }   
+
+// Comment this if it's not analytics.js
+ga('send', {
+  hitType: 'event',
+  eventCategory: eventCategory,
+  eventAction: 'Click',
+  eventLabel: eventLabel
+});
+
+setTimeout(function() {
+	window.location = href;
+}, 500);
+
+});
